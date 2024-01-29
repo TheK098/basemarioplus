8711d455413c835d7b6d09e49701a3dfa5025512
TheK098 <kushikanak@gmail.com>
2024-01-28 23:13:36 -0800

    call me a chef

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from torch.distributions import Categorical
 import torch.nn.functional as F
 import numpy as np
 import shutil
-
+import warnings
 
 def get_args():
     parser = argparse.ArgumentParser(
@@ -174,5 +174,21 @@ def train(opt):
 
 
 if __name__ == "__main__":
+    
+    
+    # Ignore specific deprecation warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning, 
+                            message=".*old step API which returns one bool instead of two.*")
+    warnings.filterwarnings("ignore", category=DeprecationWarning, 
+                            message=".*The argument mode in render method is deprecated.*")
+    warnings.filterwarnings("ignore", category=DeprecationWarning, 
+                            message=".*The torch.cuda.*DtypeTensor constructors are no longer recommended.*")
+
+    # Ignore user warnings
+    warnings.filterwarnings("ignore", category=UserWarning, 
+                            message=".*The environment SuperMarioBros-1-1-v0 is out of date.*")
+    warnings.filterwarnings("ignore", category=UserWarning, 
+                            message=".*No render modes was declared in the environment.*")
+    
     opt = get_args()
     train(opt)
